9c5a699cb741b127a31269dd6d6cc7783772876d
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2022-03-01 07:47:39 +0100

    Apply formatting

--- src/add-templated-urls.ts
+++ src/add-templated-urls.ts
@@ -1,52 +1,75 @@
-import { FileDetails, GlobOptions, getCompositeDetails, getFileDetails, getStringDetails } from "./file-details";
+import {
+  FileDetails,
+  GlobOptions,
+  getCompositeDetails,
+  getFileDetails,
+  getStringDetails,
+} from "./file-details";
 
 interface ManifestEntry {
-    url: string;
-    revision: string;
+  url: string;
+  revision: string;
 }
 
 const defaultGlobOptions: GlobOptions = {
-    globDirectory: "./",
-    globFollow: true,
-    globIgnores: ["**/node_modules/**/*"],
-    globStrict: true,
+  globDirectory: "./",
+  globFollow: true,
+  globIgnores: ["**/node_modules/**/*"],
+  globStrict: true,
 };
 
-export function addTemplatedURLs(templatedURLs: { [url: string]: string | (string[]) },
-                                 globOptions: Partial<GlobOptions> = {}): (originalManifest: ReadonlyArray<ManifestEntry>) => { manifest: ManifestEntry[]; warnings: string[] } {
-    const defaultedGlobOptions = Object.assign(defaultGlobOptions, globOptions);
+export function addTemplatedURLs(
+  templatedURLs: { [url: string]: string | string[] },
+  globOptions: Partial<GlobOptions> = {}
+): (originalManifest: ReadonlyArray<ManifestEntry>) => {
+  manifest: ManifestEntry[];
+  warnings: string[];
+} {
+  const defaultedGlobOptions = Object.assign(defaultGlobOptions, globOptions);
 
-    // See https://github.com/GoogleChrome/workbox/issues/2398#issuecomment-597080778
-    // https://github.com/GoogleChrome/workbox/blob/master/packages/workbox-build/src/lib/get-file-manifest-entries.js
+  // See https://github.com/GoogleChrome/workbox/issues/2398#issuecomment-597080778
+  // https://github.com/GoogleChrome/workbox/blob/master/packages/workbox-build/src/lib/get-file-manifest-entries.js
 
-    return (originalManifest: ReadonlyArray<ManifestEntry>): { manifest: ManifestEntry[]; warnings: string[] } => {
-        const fileDetails: FileDetails[] = [];
-        const warnings: string[] = [];
+  return (
+    originalManifest: ReadonlyArray<ManifestEntry>
+  ): { manifest: ManifestEntry[]; warnings: string[] } => {
+    const fileDetails: FileDetails[] = [];
+    const warnings: string[] = [];
 
-        for (const url of Object.keys(templatedURLs)) {
-            const dependencies = templatedURLs[url];
+    for (const url of Object.keys(templatedURLs)) {
+      const dependencies = templatedURLs[url];
 
-            if (Array.isArray(dependencies)) {
-                const details = dependencies.reduce<FileDetails[]>((accumulated, globPattern) => {
-                    const globbedFileDetails = getFileDetails(globPattern, defaultedGlobOptions);
-                    if (globbedFileDetails.length === 0) {
-                        warnings.push(`Glob pattern '${globPattern}' did not resolve to any files`);
-                    }
-                    return accumulated.concat(globbedFileDetails);
-                }, []);
-                fileDetails.push(getCompositeDetails(url, details));
-            } else {
-                fileDetails.push(getStringDetails(url, dependencies));
+      if (Array.isArray(dependencies)) {
+        const details = dependencies.reduce<FileDetails[]>(
+          (accumulated, globPattern) => {
+            const globbedFileDetails = getFileDetails(
+              globPattern,
+              defaultedGlobOptions
+            );
+            if (globbedFileDetails.length === 0) {
+              warnings.push(
+                `Glob pattern '${globPattern}' did not resolve to any files`
+              );
             }
-        }
+            return accumulated.concat(globbedFileDetails);
+          },
+          []
+        );
+        fileDetails.push(getCompositeDetails(url, details));
+      } else {
+        fileDetails.push(getStringDetails(url, dependencies));
+      }
+    }
 
-        const manifest = fileDetails.map(x => {
-            return {
-                url: x.file,
-                revision: x.hash,
-            };
-        }).concat(originalManifest);
+    const manifest = fileDetails
+      .map((x) => {
+        return {
+          url: x.file,
+          revision: x.hash,
+        };
+      })
+      .concat(originalManifest);
 
-        return { manifest, warnings };
-    };
+    return { manifest, warnings };
+  };
 }
--- src/file-details.ts
+++ src/file-details.ts
@@ -6,73 +6,80 @@ import * as upath from "upath";
 import { getDataHash, getFileHash } from "./hashing";
 
 export interface FileDetails {
-    file: string;
-    hash: string;
-    size: number;
+  file: string;
+  hash: string;
+  size: number;
 }
 
 export interface GlobOptions {
-    globDirectory: string;
-    globFollow: boolean;
-    globIgnores: string[];
-    globStrict: boolean;
+  globDirectory: string;
+  globFollow: boolean;
+  globIgnores: string[];
+  globStrict: boolean;
 }
 
 export const getStringDetails = (url: string, value: string): FileDetails => {
-    return {
-        file: url,
-        hash: getDataHash(value),
-        size: value.length,
-    };
+  return {
+    file: url,
+    hash: getDataHash(value),
+    size: value.length,
+  };
 };
 
-export const getCompositeDetails = (url: string, dependencyDetails: FileDetails[]): FileDetails => {
-    const md5 = crypto.createHash("md5");
-    let totalSize = 0;
+export const getCompositeDetails = (
+  url: string,
+  dependencyDetails: FileDetails[]
+): FileDetails => {
+  const md5 = crypto.createHash("md5");
+  let totalSize = 0;
 
-    for (const details of dependencyDetails) {
-        md5.update(details.hash);
-        totalSize += details.size;
+  for (const details of dependencyDetails) {
+    md5.update(details.hash);
+    totalSize += details.size;
+  }
+
+  const hash = md5.digest("hex");
+
+  return { file: url, hash, size: totalSize };
+};
+
+export const getFileDetails = (
+  globPattern: string,
+  globOptions: GlobOptions
+): FileDetails[] => {
+  // See https://github.com/GoogleChrome/workbox/blob/master/packages/workbox-build/src/lib/get-file-details.js
+
+  const globbedFiles = glob.sync(globPattern, {
+    cwd: globOptions.globDirectory,
+    follow: globOptions.globFollow,
+    ignore: globOptions.globIgnores,
+    strict: globOptions.globStrict,
+  });
+
+  const getFileSize = (file: string): number | null => {
+    const stat = fs.statSync(file);
+    if (!stat.isFile()) {
+      return null;
+    }
+    return stat.size;
+  };
+
+  const fileDetails = globbedFiles.map((file) => {
+    const fullPath = upath.join(globOptions.globDirectory, file);
+    const size = getFileSize(fullPath);
+    if (size === null) {
+      return null;
     }
 
-    const hash = md5.digest("hex");
+    const hash = getFileHash(fullPath);
 
-    return { file: url, hash, size: totalSize };
-};
+    return { file, hash, size };
+  });
 
-export const getFileDetails = (globPattern: string, globOptions: GlobOptions): FileDetails[] => {
-    // See https://github.com/GoogleChrome/workbox/blob/master/packages/workbox-build/src/lib/get-file-details.js
+  // If !== null, means it's a valid file.
+  const predicate = (details: FileDetails | null): details is FileDetails =>
+    !!details;
+  const globbedFileDetails = fileDetails.filter(predicate);
 
-    const globbedFiles = glob.sync(globPattern, {
-        cwd: globOptions.globDirectory,
-        follow: globOptions.globFollow,
-        ignore: globOptions.globIgnores,
-        strict: globOptions.globStrict,
-    });
-
-    const getFileSize = (file: string): number | null => {
-        const stat = fs.statSync(file);
-        if (!stat.isFile()) {
-            return null;
-        }
-        return stat.size;
-    };
-
-    const fileDetails = globbedFiles.map((file) => {
-        const fullPath = upath.join(globOptions.globDirectory, file);
-        const size = getFileSize(fullPath);
-        if (size === null) {
-            return null;
-        }
-
-        const hash = getFileHash(fullPath);
-
-        return { file, hash, size };
-    });
-
-    // If !== null, means it's a valid file.
-    const predicate = (details: FileDetails | null): details is FileDetails => !!details;
-    const globbedFileDetails = fileDetails.filter(predicate);
-
-    return globbedFileDetails;
+  return globbedFileDetails;
 };
